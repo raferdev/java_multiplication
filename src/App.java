003cb4589624fa930d89f01a357cc6c4739b3bec
Rafael <rafinha_fern@hotmail.com>
2023-01-10 14:48:18 -0300

    feat: new multiplication table with number input

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Enter with a int number");
+        Scanner number = new Scanner(System.in);
+        int x = number.nextInt();
+
+        System.out.println("Multiplication Table of " + x);
+        for (int i = 0; i <= x; i++) {
+            int mult = i * x;
+            String text = i + " x " + x + " = " + mult;
+            System.out.println(text);
+        }
     }
 }
